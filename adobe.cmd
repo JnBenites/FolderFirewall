fd85301a1cb5a29bd822de273849748ea7a684dc
Juan B <55644440+JnBenites@users.noreply.github.com>
2024-05-28 07:15:18 -0500

    Update adobe.cmd

--- adobe.cmd
+++ adobe.cmd
@@ -1,5 +1,8 @@
-for /r "C:\Program Files\Adobe" %%F in (*.exe) do (
-    echo %%F
+@echo off
+set /p directory=Enter the directory path (e.g., C:\Program Files\Adobe): 
+
+for /r "%directory%" %%F in (*.exe) do (
+    echo Blocking %%F
     netsh advfirewall firewall add rule name="%%~nxF_out" dir=out action=block program="%%F" enable=yes
     netsh advfirewall firewall add rule name="%%~nxF_in" dir=in action=block program="%%F" enable=yes
 )
